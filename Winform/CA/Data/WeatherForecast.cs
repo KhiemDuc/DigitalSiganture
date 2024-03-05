8d9b9b7353815aa9e8908c782dd261c252c3eeb2
Duck <khiemthao2810@gmail.com>
2024-03-06 00:12:50 +0700

    init http request data

--- Winform/CA/Data/WeatherForecast.cs
+++ Winform/CA/Data/WeatherForecast.cs
@@ -1,13 +1,28 @@
+﻿using Newtonsoft.Json;
+
 namespace CA.Data
 {
-    public class WeatherForecast
+    public class Transaction
     {
-        public DateTime Date { get; set; }
+        [JsonProperty("Mã GD")]
+        public int MaGD { get; set; }
 
-        public int TemperatureC { get; set; }
+        [JsonProperty("Mô tả")]
+        public string MoTa { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        [JsonProperty("Giá trị")]
+        public int GiaTri { get; set; }
 
-        public string Summary { get; set; }
+        [JsonProperty("Ngày diễn ra")]
+        public string NgayDienRa { get; set; }
+
+        [JsonProperty("Số tài khoản")]
+        public string SoTaiKhoan { get; set; }
+    }
+
+    public class Response
+    {
+        public List<Transaction> data { get; set; }
+        public bool error { get; set; }
     }
 }
--- Winform/CA/Data/WeatherForecastService.cs
+++ Winform/CA/Data/WeatherForecastService.cs
@@ -1,20 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+
 namespace CA.Data
 {
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly HttpClient client = new HttpClient();
+        JsonSerializerOptions _serializerOptions;
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public async Task<Response> GetForecastAsync()
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            _serializerOptions = new JsonSerializerOptions();
+            try
             {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+                HttpResponseMessage response = await client.GetAsync("https://script.google.com/macros/s/AKfycbyRsK4LUEB0RqmZKn41y85w8H_BiUZBT0GdSEUfOVfDf7ZBEEeEZPE4o5sIXfNeBrN3zQ/exec");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Parse the response body
+                var data = new Response();
+                try
+                {
+                    data = JsonConvert.DeserializeObject<Response>(responseBody);
+                }
+                catch (System.Text.Json.JsonException ex)
+                {
+                    Console.WriteLine($"An error occurred while deserializing the response body: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                }
+                return data;
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
         }
     }
 
