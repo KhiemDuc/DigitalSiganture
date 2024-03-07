423164f6e30b393fe4b649082d2581bfc3d85184
Duck <khiemthao2810@gmail.com>
2024-03-08 00:24:00 +0700

    add sign in and sign up

--- Frontend/digital-signature/src/App.js
+++ Frontend/digital-signature/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import LandingPage from './LandingPage';
+import SignIn from './pages/sign_in/SignIn';
+import SignUp from './pages/sign_up/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/sign_in' element={<SignIn />} />
+          <Route path='/sign_up' element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
